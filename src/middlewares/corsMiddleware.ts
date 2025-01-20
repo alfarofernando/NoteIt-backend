1526016832df506fa72a7b07038b3782053fbf1c
Fernando Alfaro <alfarofernando@live.com>
2025-01-20 03:19:51 -0300

    je
    je

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,7 +1,7 @@
 import cors, { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: ['http://localhost:5173', 'https://noteit-frontend.netlify.app/'], // Orígenes permitidos
+  origin: ['http://localhost:5173', 'https://noteit-frontend.netlify.app'], // Orígenes permitidos
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
   credentials: true, // Permitir cookies o encabezados de autenticación

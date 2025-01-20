c8fbfc88e5daf3ccf9f79a4f2fc2885975319957
Fernando Alfaro <alfarofernando@live.com>
2025-01-20 03:47:51 -0300

    Update corsMiddleware.ts

--- src/middlewares/corsMiddleware.ts
+++ src/middlewares/corsMiddleware.ts
@@ -1,8 +1,8 @@
 import cors, { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: ['http://localhost:5173', 'https://noteit-frontend.netlify.app'], // Orígenes permitidos
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+  origin: ['https://noteit-frontend.netlify.app/'], // Orígenes permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Métodos HTTP permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
   credentials: true, // Permitir cookies o encabezados de autenticación
 };
